f681f8728c960ef4d6327ba4c80f960466f6b841
smwsl <smwsl@example.com>
2025-11-23 21:42:49 +0330

    fix: new text color based on the theme

--- src/components/editor-tabs/type/typography-list.tsx
+++ src/components/editor-tabs/type/typography-list.tsx
@@ -1,15 +1,19 @@
 import { Type } from 'lucide-react';
 import { memo } from 'react';
+import { useTheme } from 'next-themes';
 import { useShallow } from 'zustand/shallow';
 import { Button } from '~/components/ui/button';
 import { useEditorStore } from '~/store/editor.store';
 
 const TypographyList = memo(() => {
+  const { resolvedTheme } = useTheme();
   const { addWidget } = useEditorStore(
     useShallow((state) => ({
       addWidget: state.addWidget,
     }))
   );
+
+  const textColor = resolvedTheme === 'dark' ? '#ffffff' : '#000000';
 
   return (
     <div>
@@ -18,7 +22,7 @@ const TypographyList = memo(() => {
           addWidget<'text'>({
             type: 'text',
             text: 'New text',
-            fill: '#000000',
+            fill: textColor,
             fontSize: 48,
             align: 'left',
             shadowColor: '#000000',
